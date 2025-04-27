deeecf3f714939f22676443cd9863454253809bf
webgis01 <webgisthesis@gmail.com>
2025-04-27 21:50:41 +0800

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+if len(distances_field2) > 0:
+    #     forecast_list = perform_smoothing("device2", distances_field2, forecast_list)
+    # if len(distances_field3) > 0:
+    #     forecast_list = perform_smoothing("device3", distances_field3, forecast_list)
+    # if len(distances_field4) > 0:
+    #     forecast_list = perform_smoothing("device4", distances_field4, forecast_list)
